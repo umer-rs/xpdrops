b5d4c4f44fdfc296bdce54e43699f1cbcd1f75fc
umer-rs <72721490+umer-rs@users.noreply.github.com>
2024-07-16 09:53:17 +0200

    make tekton def more correct

--- src/main/java/com/xpdrops/predictedhit/npcswithscalingbonus/cox/Tekton.java
+++ src/main/java/com/xpdrops/predictedhit/npcswithscalingbonus/cox/Tekton.java
@@ -2,9 +2,24 @@ package com.xpdrops.predictedhit.npcswithscalingbonus.cox;
 
 public class Tekton extends CoXNPC
 {
+	private int scaledPartySize = 1;
+
 	public Tekton(int hp, int att, int str, int mage, int range, int def, int offensiveAtt, int offensiveStr, int defensiveStab, int defensiveSlash, int defensiveCrush, int defensiveMage, int defensiveRange)
 	{
 		super(hp, att, str, mage, range, def, offensiveAtt, offensiveStr, defensiveStab, defensiveSlash, defensiveCrush, defensiveMage, defensiveRange);
+	}
+
+	@Override
+	protected double calculateHpScaling(int scaledPartySize, int playersInRaid)
+	{
+		this.scaledPartySize = scaledPartySize;
+		return super.calculateHpScaling(scaledPartySize, playersInRaid);
+	}
+
+	@Override
+	protected double cmDefenceMultiplier()
+	{
+		return scaledPartySize < 4 ? 1.2 : 1.35;
 	}
 
 	@Override
